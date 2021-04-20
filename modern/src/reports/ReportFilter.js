7791af5257657cc19eca78c6e510590944022d69
Jithin <jithin@space-kerala.org>
2021-04-20 18:53:32 +0530

    test

--- modern/src/reports/ReportFilter.js
+++ modern/src/reports/ReportFilter.js
@@ -1,43 +1,52 @@
-import React, { useState } from 'react';
-import { FormControl, InputLabel, Select, MenuItem, Button, TextField, ButtonGroup } from '@material-ui/core';
-import t from '../common/localization';
-import { useSelector } from 'react-redux';
-import moment from 'moment';
+import React, { useState } from "react";
+import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  ButtonGroup,
+} from "@material-ui/core";
+import t from "../common/localization";
+import { useSelector } from "react-redux";
+import moment from "moment";
 
 const ReportFilter = ({ children, handleSubmit, showOnly }) => {
-  const devices = useSelector(state => Object.values(state.devices.items));
+  const devices = useSelector((state) => Object.values(state.devices.items));
   const [deviceId, setDeviceId] = useState();
-  const [period, setPeriod] = useState('today');
-  const [from, setFrom] = useState(moment().subtract(1, 'hour'));
+  const [period, setPeriod] = useState("today");
+  const [from, setFrom] = useState(moment().subtract(1, "hour"));
   const [to, setTo] = useState(moment());
 
   const handleClick = (mail, json) => {
     let selectedFrom;
     let selectedTo;
     switch (period) {
-      case 'today':
-        selectedFrom = moment().startOf('day');
-        selectedTo = moment().endOf('day');
+      case "today":
+        selectedFrom = moment().startOf("day");
+        selectedTo = moment().endOf("day");
         break;
-      case 'yesterday':
-        selectedFrom = moment().subtract(1, 'day').startOf('day');
-        selectedTo = moment().subtract(1, 'day').endOf('day');
+      case "yesterday":
+        selectedFrom = moment().subtract(1, "day").startOf("day");
+        selectedTo = moment().subtract(1, "day").endOf("day");
         break;
-      case 'thisWeek':
-        selectedFrom = moment().startOf('week');
-        selectedTo = moment().endOf('week');
+      case "thisWeek":
+        selectedFrom = moment().startOf("week");
+        selectedTo = moment().endOf("week");
         break;
-      case 'previousWeek':
-        selectedFrom = moment().subtract(1, 'week').startOf('week');
-        selectedTo = moment().subtract(1, 'week').endOf('week');
+      case "previousWeek":
+        selectedFrom = moment().subtract(1, "week").startOf("week");
+        selectedTo = moment().subtract(1, "week").endOf("week");
         break;
-      case 'thisMonth':
-        selectedFrom = moment().startOf('month');
-        selectedTo = moment().endOf('month');
+      case "thisMonth":
+        selectedFrom = moment().startOf("month");
+        selectedTo = moment().endOf("month");
         break;
-      case 'previousMonth':
-        selectedFrom = moment().subtract(1, 'month').startOf('month');
-        selectedTo = moment().subtract(1, 'month').endOf('month');
+      case "previousMonth":
+        selectedFrom = moment().subtract(1, "month").startOf("month");
+        selectedTo = moment().subtract(1, "month").endOf("month");
         break;
       default:
         selectedFrom = from;
@@ -45,7 +54,9 @@ const ReportFilter = ({ children, handleSubmit, showOnly }) => {
         break;
     }
 
-    const accept = json ? 'application/json' : 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+    const accept = json
+      ? "application/json"
+      : "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
     handleSubmit(
       deviceId,
       selectedFrom.toISOString(),
@@ -53,12 +64,12 @@ const ReportFilter = ({ children, handleSubmit, showOnly }) => {
       mail,
       { Accept: accept }
     );
-  }
+  };
 
   return (
     <>
       <FormControl variant="filled" margin="normal" fullWidth>
-        <InputLabel>{t('reportDevice')}</InputLabel>
+        <InputLabel>{t("reportDevice")}</InputLabel>
         <Select value={deviceId} onChange={(e) => setDeviceId(e.target.value)}>
           {devices.map((device) => (
             <MenuItem value={device.id}>{device.name}</MenuItem>
@@ -66,47 +77,69 @@ const ReportFilter = ({ children, handleSubmit, showOnly }) => {
         </Select>
       </FormControl>
       <FormControl variant="filled" margin="normal" fullWidth>
-        <InputLabel>{t('reportPeriod')}</InputLabel>
+        <InputLabel>{t("reportPeriod")}</InputLabel>
         <Select value={period} onChange={(e) => setPeriod(e.target.value)}>
-          <MenuItem value="today">{t('reportToday')}</MenuItem>
-          <MenuItem value="yesterday">{t('reportYesterday')}</MenuItem>
-          <MenuItem value="thisWeek">{t('reportThisWeek')}</MenuItem>
-          <MenuItem value="previousWeek">{t('reportPreviousWeek')}</MenuItem>
-          <MenuItem value="thisMonth">{t('reportThisMonth')}</MenuItem>
-          <MenuItem value="previousMonth">{t('reportPreviousMonth')}</MenuItem>
-          <MenuItem value="custom">{t('reportCustom')}</MenuItem>
+          <MenuItem value="today">{t("reportToday")}</MenuItem>
+          <MenuItem value="yesterday">{t("reportYesterday")}</MenuItem>
+          <MenuItem value="thisWeek">{t("reportThisWeek")}</MenuItem>
+          <MenuItem value="previousWeek">{t("reportPreviousWeek")}</MenuItem>
+          <MenuItem value="thisMonth">{t("reportThisMonth")}</MenuItem>
+          <MenuItem value="previousMonth">{t("reportPreviousMonth")}</MenuItem>
+          <MenuItem value="custom">{t("reportCustom")}</MenuItem>
         </Select>
       </FormControl>
-      {period === 'custom' && (
-        <TextField
-          margin="normal"
-          variant="filled"
-          label={t('reportFrom')}
-          type="datetime-local"
-          value={from.format(moment.HTML5_FMT.DATETIME_LOCAL)}
-          onChange={e => setFrom(moment(e.target.value, moment.HTML5_FMT.DATETIME_LOCAL))}
-          fullWidth />
-      )}
-      {period === 'custom' && (
-        <TextField
-          margin="normal"
-          variant="filled"
-          label={t('reportTo')}
-          type="datetime-local"
-          value={to.format(moment.HTML5_FMT.DATETIME_LOCAL)}
-          onChange={e => setTo(moment(e.target.value, moment.HTML5_FMT.DATETIME_LOCAL))}
-          fullWidth />
-      )}
+      <MuiPickersUtilsProvider libInstance={moment} utils={MomentUtils}>
+        {period === "custom" && (
+          <DateTimePicker
+            ampm={false}
+            label={t("reportFrom")}
+            showTodayButton
+            fullWidth
+            inputVariant="filled"
+            value={from.format(moment.HTML5_FMT.DATETIME_LOCAL)}
+            onChange={(date) =>
+              setFrom(moment(date, moment.HTML5_FMT.DATETIME_LOCAL))
+            }
+          />
+        )}{" "}
+        &nbsp;
+        {period === "custom" && (
+          <DateTimePicker
+            ampm={false}
+            label={t("reportTo")}
+            showTodayButton
+            inputVariant="filled"
+            value={to.format(moment.HTML5_FMT.DATETIME_LOCAL)}
+            onChange={(date) =>
+              setTo(moment(date, moment.HTML5_FMT.DATETIME_LOCAL))
+            }
+          />
+        )}
+      </MuiPickersUtilsProvider>
       {children}
       <FormControl margin="normal" fullWidth>
-        <ButtonGroup color="primary" orientation="vertical" disabled={!deviceId}>
-          <Button onClick={() => handleClick(false, true)}>{t('reportShow')}</Button>
-          {!showOnly && <Button onClick={() => handleClick(false, false)}>{t('reportExport')}</Button>}
-          {!showOnly && <Button onClick={() => handleClick(true, false)}>{t('reportEmail')}</Button>}
+        <ButtonGroup
+          color="primary"
+          orientation="vertical"
+          disabled={!deviceId}
+        >
+          <Button onClick={() => handleClick(false, true)}>
+            {t("reportShow")}
+          </Button>
+          {!showOnly && (
+            <Button onClick={() => handleClick(false, false)}>
+              {t("reportExport")}
+            </Button>
+          )}
+          {!showOnly && (
+            <Button onClick={() => handleClick(true, false)}>
+              {t("reportEmail")}
+            </Button>
+          )}
         </ButtonGroup>
       </FormControl>
     </>
   );
-}
+};
 
 export default ReportFilter;
--- modern/src/reports/SpeedReportPage.js
+++ modern/src/reports/SpeedReportPage.js
@@ -1,77 +1,251 @@
-import React, { useState } from 'react';
-import { TableContainer, Table, TableRow, TableCell, TableHead, TableBody, Paper } from '@material-ui/core';
-import { DataGrid } from '@material-ui/data-grid';
-import t from '../common/localization';
-import { formatDistance, formatSpeed, formatHours, formatDateTime } from '../common/formatter';
-import ReportFilter from './ReportFilter';
-import ReportLayoutPage from './ReportLayoutPage';
-import { useAttributePreference } from '../common/preferences';
+import React, { useState } from "react";
+import axios from "../lib/axios";
+import {
+  TableContainer,
+  Table,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableBody,
+  Paper,
+} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarExport,
+} from "@material-ui/data-grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Badge from "@material-ui/core/Badge";
 
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+
+import t from "../common/localization";
+import {
+  formatDistance,
+  formatSpeed,
+  formatHours,
+  formatDateTime,
+} from "../common/formatter";
+import ReportFilter from "./ReportFilter";
+import ReportLayoutPage from "./ReportLayoutPage";
+import { useAttributePreference } from "../common/preferences";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "grid",
+    gridTemplateColumns: "repeat(12, 1fr)",
+    gridGap: theme.spacing(3),
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    whiteSpace: "nowrap",
+    marginBottom: theme.spacing(1),
+  },
+  divider: {
+    margin: theme.spacing(2, 0),
+  },
+}));
 const Filter = ({ setItems }) => {
-
   const handleSubmit = async (deviceId, from, to, mail, headers) => {
     const query = new URLSearchParams({ deviceId, from, to, mail });
-    const response = await fetch(`/fn-api/positions?${query.toString()}`, { headers });
-    if (response.ok) {
-      setItems(await response.json());
-    }
-  }
+    axios
+      .post("/fn-api/positions", {
+        deviceId,
+        from,
+        to,
+      })
+      .then((res) => {
+        setItems(res.data);
+      })
+      .catch(console.error);
+  };
 
-  return <ReportFilter handleSubmit={handleSubmit} />;
+  return <ReportFilter handleSubmit={handleSubmit} showOnly={true} />;
+};
+
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer>
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
 }
-
 const SpeedReportPage = () => {
-  
-  const distanceUnit = useAttributePreference('distanceUnit');
-  const speedUnit = useAttributePreference('speedUnit');
+  const distanceUnit = useAttributePreference("distanceUnit");
+  const speedUnit = useAttributePreference("speedUnit");
   const [items, setItems] = useState([]);
+  const classes = useStyles();
 
   const columns = [
-    { field: 'fixtime', headerName: 'Date Time', flex:1.5,
-      valueFormatter: (p) => formatDateTime(p.value)
+    {
+      field: "fixtime",
+      headerName: "Date Time",
+      flex: 1.5,
+      valueFormatter: (p) => formatDateTime(p.value),
     },
-    { field: 'speed', headerName: 'Speed', flex:1,
-      valueFormatter: (p) => formatSpeed(p.value, 'kmh'),
+    {
+      field: "speed",
+      headerName: "Speed",
+      flex: 1,
+      valueFormatter: (p) => formatSpeed(p.value, "kmh"),
     },
-    { field: 'longitude', headerName: 'Longitude', flex:1},
-    { field: 'latitude', headerName: 'Latitude', flex:1},
-    { field: 'address', headerName: 'Position', flex:1},
-    { field: 'overspeed', headerName: 'Over Speed', flex:1,
-      valueGetter: (p) => ((p.getValue('speed') || 0 )* 1.852).toFixed(2) < 80 ? 'No': 'YES'
+    { field: "longitude", headerName: "Longitude", flex: 1 },
+    { field: "latitude", headerName: "Latitude", flex: 1 },
+    { field: "address", headerName: "Position", flex: 1 },
+    {
+      field: "overspeed",
+      headerName: "Over Speed",
+      flex: 1,
+      valueGetter: (p) =>
+        ((p.getValue("speed") || 0) * 1.852).toFixed(2) < 80 ? "No" : "YES",
     },
-  ]
+  ];
 
   return (
     <ReportLayoutPage filter={<Filter setItems={setItems} />}>
+      <Grid container spacing={3}>
+        <Grid item xs={8}>
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant="h6" component="h4">
+                  Overview
+                </Typography>
+                <Grid container spacing={0}>
+                  <Grid item xs={12} md={4}>
+                    <div className={classes.demo}>
+                      <List dense>
+                        <ListItem>
+                          <ListItemText
+                            primary="Vehicle Owner Name"
+                            secondary="Jithin Thankachan"
+                          />
+                        </ListItem>
+                        <ListItem>
+                          <ListItemText
+                            primary="Phone Number"
+                            secondary="N/A"
+                          />
+                        </ListItem>
+                      </List>
+                    </div>
+                  </Grid>
+                  <Grid item xs={12} md={4}>
+                    <div className={classes.demo}>
+                      <List dense>
+                        <ListItem>
+                          <ListItemText
+                            primary="Vehicle Reg Num"
+                            secondary="KL-69-A-9246"
+                          />
+                        </ListItem>
+                        <ListItem>
+                          <ListItemText
+                            primary="Vehicle Engine Num"
+                            secondary="N/A"
+                          />
+                        </ListItem>
+                      </List>
+                    </div>
+                  </Grid>
+                  <Grid item xs={12} md={4}>
+                    <div className={classes.demo}>
+                      <List dense>
+                        <ListItem>
+                          <ListItemText primary="Vehicle ID" secondary="1234" />
+                        </ListItem>
+                        <ListItem>
+                          <ListItemText
+                            primary="Vehicle Status"
+                            secondary="online"
+                          />
+                        </ListItem>
+                      </List>
+                    </div>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant="h6" component="h4">
+                  Summary
+                </Typography>
+                <Grid container spacing={0}>
+                  <Grid item xs={12} md={6}>
+                    <div className={classes.demo}>
+                      <List dense>
+                        <ListItem>
+                          <ListItemText primary="Set Speed" />
+                          <ListItemAvatar>
+                            <Avatar>80</Avatar>
+                          </ListItemAvatar>
+                        </ListItem>
+                        <ListItem>
+                          <ListItemText primary="Max Speed" />
+                          <ListItemAvatar>
+                            <Avatar>140</Avatar>
+                          </ListItemAvatar>
+                        </ListItem>
+                      </List>
+                    </div>
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <div className={classes.demo}>
+                      <List dense>
+                        <ListItem>
+                          <ListItemText primary="Avg Speed" />
+                          <ListItemAvatar>
+                            <Avatar>20</Avatar>
+                          </ListItemAvatar>
+                        </ListItem>
+                        <ListItem>
+                          <ListItemText primary="Over Speed Events" />
+                          <ListItemAvatar>
+                            <Avatar>3</Avatar>
+                          </ListItemAvatar>
+                        </ListItem>
+                      </List>
+                    </div>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
       <TableContainer component={Paper}>
-        <DataGrid autoHeight={'true'} rows={items} columns={columns} pageSize={5} width={'100%'} />
-        {/* <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>{'Date Time'}</TableCell>
-              <TableCell>{'Speed'}</TableCell>
-              <TableCell>{'Longitude'}</TableCell>
-              <TableCell>{'Latitude'}</TableCell>
-              <TableCell>{'Position'}</TableCell>
-              <TableCell>{'Speed Status'}</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {items.map((item) => (
-              <TableRow key={item.id}>
-                <TableCell>{formatDateTime(item.fixtime)}</TableCell>
-                <TableCell>{formatSpeed(item.speed, 'kmh')}</TableCell>
-                <TableCell>{item.longitude}</TableCell>
-                <TableCell>{item.latitude}</TableCell>
-                <TableCell>{item.address}</TableCell>
-                <TableCell>{(item.speed < 80 ? 'Regular Speed': 'Over Speed')}</TableCell>                             
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table> */}
+        <DataGrid
+          autoHeight={"true"}
+          rows={items}
+          columns={columns}
+          pageSize={5}
+          width={"100%"}
+          components={{ Toolbar: CustomToolbar }}
+        />
       </TableContainer>
     </ReportLayoutPage>
   );
-}
+};
 
 export default SpeedReportPage;
